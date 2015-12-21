31250eea11739e30a64968394ecf65bb25d4793d
Raymond Tejano <rtejano@gmail.com>
2015-12-21 16:39:35 +0800

    Added new static/image directory - add image - modified the template to reflect the image

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 
 from django.conf import settings
-from django.conf.urls.static import static # New Import
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
 urlpatterns = [
