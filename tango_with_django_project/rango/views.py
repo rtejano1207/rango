115096596eac25e8b4cf96c6eca9b2891db349f8
Raymond Tejano <rtejano@gmail.com>
2015-12-21 21:47:23 +0800

    Added new static/image directory - add image - modified the template to reflect the image

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -17,4 +17,17 @@ def index(request):
      return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page")
+    # construct own dictionary to pass to the template engine
+    return HttpResponse("")
+
+def about_template(request):
+    #RRT 12.21.2015 - commented out so to try own template for about page
+
+    # construct a dictionary to pass to the template engine as its context
+    context_dict = {'italicmessage': "I am italicized font from the context"}
+
+    # return a renedered response to send to the client
+    # we make us of the shortcut function to make our lives easier.
+    # note that the first parameter is that same {{ italicmessage }} in the template!
+    return render(request, 'rango/about.html',context_dict)
+
